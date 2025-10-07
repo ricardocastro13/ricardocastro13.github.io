9af3b956bb1809f159083cacf201dab2109f6ced
ricardocastro13 <a22402222@alunos.ulht.pt>
2025-10-07 11:46:55 +0000

    gravado

--- lab4/main.js
+++ lab4/main.js
@@ -1,15 +1,14 @@
 let contador = 0;
 
-// Mostra um alerta
+
 function saudar() {
-  alert("Olá!");
+  alert("Olaa!");
 }
 
-// Muda o texto do h1
 function mudarTexto() {
   const titulo = document.querySelector("#titulo");
   if (titulo.textContent === "Olá!") {
-    titulo.textContent = "Adeus!";
+    titulo.textContent = "xau!";
     titulo.style.color = "green";
   } else {
     titulo.textContent = "Olá!";
@@ -17,24 +16,24 @@ function mudarTexto() {
   }
 }
 
-// Muda a cor de fundo
+
 function mudarCor() {
   document.body.style.backgroundColor = "lightblue";
 }
 
-// Quando o rato passa por cima
+
 function destacar() {
   const msg = document.querySelector("#mensagem");
   msg.textContent = "Estás com o rato sobre o botão!";
 }
 
-// Quando o rato sai
+
 function retirarDestaque() {
   const msg = document.querySelector("#mensagem");
-  msg.textContent = "O rato saiu do botão.";
+  msg.textContent = "O rato saiu do botão";
 }
 
-// Contador simples
+
 function contar() {
   contador++;
   const texto = document.querySelector("#contador");
